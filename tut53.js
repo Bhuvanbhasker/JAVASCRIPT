9879e1c28780fd1ff204599ef790d8c6ccbbb039
Bhuvanbhasker <bhuvansingh342002@gmail.com>
2023-09-29 09:58:18 +0530

    first commit

---
+++ tut53.js
@@ -0,0 +1,19 @@
+console.log("this is a tutorial 53")
+
+function greet(name, greetText) {
+    console.log(greetText + " " + name);
+    console.log(name + "is a good boy");
+}
+let name = "Harry";
+let name1 = "rohit";
+let name2 = "arun";
+let name3 = "aman";
+let greetText = "Good morning";
+greet(name, greetText);
+greet(name2, greetText1);
+greet(name3, greetText2);
+greet(name4, greetText3);
+//console.log(name + " is a good boy");
+//console.log(name1 + " is a good boy");
+//console.log(name2 + " is a good boy");
+//console.log(name3 + " is a good boy");
